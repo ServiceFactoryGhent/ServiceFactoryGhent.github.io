050168c5d58b1701d097c62efc7930a26aff83d8
Van Driessche Maxime <van-driessche-maxime@hotmail.com>
2017-11-07 16:31:33 +0100

    home page and nav-bar work properly on internet explorer

--- js/main.js
+++ js/main.js
@@ -1,60 +1,71 @@
- //Watch on scroll and update nav
- var logo = document.getElementsByClassName('navbar-brand')[0];
- var nav = document.getElementsByTagName('header')[0];
- var bars = document.querySelectorAll('span.icon-bar');
+//Watch on scroll and update nav
+/*jslint browser:true */
+var logo = document.getElementsByClassName("navbar-brand")[0];
+var nav = document.getElementsByTagName("header")[0];
+var bars = document.querySelectorAll("span.icon-bar");
+var hamburger = document.getElementsByClassName("navbar-toggle collapsed")[0];
+var navbar = document.getElementById("main-navbar");
 
- var hamburger = document.getElementsByClassName('navbar-toggle collapsed')[0];
- var navbar = document.getElementById('main-navbar');
+function hasClass(element, className) {
+    "use strict";
+    return (" " + element.className + " ").indexOf(" " + className + " ") > -1;
+}
 
- Element.prototype.hasClass = function(className) {
-     return new RegExp(' ' + className + ' ').test(' ' + this.className + ' ');
- };
- Element.prototype.addClass = function(className) {
-     if (!this.hasClass(className)) {
-         this.className += ' ' + className;
-     }
- };
- Element.prototype.removeClass = function(className) {
-     var newClass = ' ' + this.className.replace( /[\t\r\n]/g, ' ') + ' ';
-     if (this.hasClass(className)) {
-         while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-             newClass = newClass.replace(' ' + className + ' ', ' ');
-         }
-         this.className = newClass.replace(/^\s+|\s+$/g, '');
-     }
- };
- Element.prototype.toggleClass = function(className) {
-	var newClass = ' ' + this.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (this.hasClass(className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        this.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        this.className += ' ' + className;
+function addClass(element, className) {
+    "use strict";
+    if (!hasClass(element, className)) {
+        element.className += " " + className;
     }
-};
+}
 
- window.addEventListener('scroll', function() {
-     logo.addClass('default-logo');
-     navbar.removeClass('navbar-white');
-     nav.style.backgroundColor = "rgba(255,255,255,.97)";
-     nav.style.boxShadow = "1px 1px 5px grey";
-     for (var i = 0; i <= 2; i++) {
-         bars[i].style.backgroundColor = "#006495";
-     }
-     if (!window.scrollY >= 1) {
-         logo.removeClass('default-logo');
-         navbar.addClass('navbar-white');
-         nav.style.backgroundColor = "transparent";
-         nav.style.boxShadow = "none";
-         for (var i = 0; i <= 2; i++) {
-             bars[i].style.backgroundColor = "#FFF";
-         }
-     }
- });
- hamburger.addEventListener("click", function() {
-     hamburger.toggleClass('collapsed');
-     navbar.toggleClass('in');
-     navbar.style.transition = "height 258ms ease-out";
- });
+function removeClass(element, className) {
+    "use strict";
+    var newClass = " " + element.className.replace(/[\t\r\n]/g, " ") + " ";
+    if (hasClass(element, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        element.className = newClass.replace(/^\s+|\s+$/g, "");
+    }
+}
+
+function toggleClass(element, className) {
+    "use strict";
+    var newClass = " " + element.className.replace(/[\t\r\n]/g, " ") + " ";
+    if (element.hasClass(className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        element.className = newClass.replace(/^\s+|\s+$/g, "");
+    } else {
+        element.className += " " + className;
+    }
+}
+
+window.addEventListener("scroll", function () {
+    "use strict";
+    var i;
+    addClass(logo, "default-logo");
+    removeClass(navbar, "navbar-white");
+    nav.style.backgroundColor = "rgba(255,255,255,.97)";
+    nav.style.boxShadow = "1px 1px 5px grey";
+    for (i = 0; i <= 2; i += 1) {
+        bars[i].style.backgroundColor = "#006495";
+    }
+    if (!(window.pageYOffset >= 1)) {
+        removeClass(logo, "default-logo");
+        addClass(navbar, "navbar-white");
+        nav.style.backgroundColor = "transparent";
+        nav.style.boxShadow = "none";
+        for (i = 0; i <= 2; i += 1) {
+            bars[i].style.backgroundColor = "#FFF";
+        }
+    }
+});
+
+hamburger.addEventListener("click", function () {
+    "use strict";
+    toggleClass(hamburger, "collapsed");
+    toggleClass(navbar, "in");
+    navbar.style.transition = "height 258ms ease-out";
+});
